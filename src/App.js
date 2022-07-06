1ac6fa76b76f59ae3e4b605cda6f980afc46cd87
Talisker25 <cth8787@gmail.com>
2022-07-06 22:57:21 +0900

    6-4 clean up 완료

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
-import Button from "./Button";
-import styles from "./App.module.css"
 import {useState, useEffect} from "react";
 
-function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setValue(prevState => prevState + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-    console.log("i run all time");
-    useEffect(() => {
-        console.log("i run only once...");
+function Hello(){
+    useEffect(function() {
+        console.log("hi :)");
+        return () => console.log("bye :(");
     }, []);
-    useEffect(() => {
-        if(keyword !== "" && keyword.length > 5) {
-            console.log("SEARCH FOR", keyword);
-        }
-    }, [keyword]);
-    useEffect(() => {
-        if(counter > 0){
-            console.log("Counter increase");
-        }
-    }, [counter]);
-    useEffect(() => {
-        console.log("keyword and counter change");
-    }, [keyword, counter]);
+    return <h1>Hello</h1>
+}
+
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => {
+        setShowing(prevState => !prevState);
+    }
     return (
         <div>
-            <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."/>
-            <h1 className={styles.title}>{counter}</h1>
-            <Button text={"click me"} onClickFunc={onClick}/>
+            {showing ? <Hello/> : null}
+            <button onClick={onClick}>
+                {showing ? "hide" : "show"}
+            </button>
         </div>
     );
 }
